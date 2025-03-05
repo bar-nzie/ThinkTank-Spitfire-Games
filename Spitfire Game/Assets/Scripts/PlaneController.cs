1a15434a8be805ad2051bc6fa106165e38d8210b
James Roper <145114710+WookiePub3@users.noreply.github.com>
2025-03-05 10:47:21 +0000

    merged and sorted
    rolled and smoked

--- Spitfire Game/Assets/Scripts/PlaneController.cs
+++ Spitfire Game/Assets/Scripts/PlaneController.cs
@@ -1,164 +1,164 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+//using UnityEngine.InputSystem;
 
-namespace Sean
-{
-    public class planeMovement : MonoBehaviour
-    {
+//namespace Sean
+//{
+//    public class planeMovement : MonoBehaviour
+//    {
 
-        [SerializeField] private InputActionReference left, right, shoot;
-        [SerializeField] private float speed;
+//        [SerializeField] private InputActionReference left, right, shoot;
+//        [SerializeField] private float speed;
 
-        private bool _moveLeft;
-        private bool _moveRight;
-        private bool _moveUp;
-        private bool _moveDown;
-        private bool _isShooting;
+//        private bool _moveLeft;
+//        private bool _moveRight;
+//        private bool _moveUp;
+//        private bool _moveDown;
+//        private bool _isShooting;
 
-        private float _smooth = 5.0f;
-        private float tiltAngle = 50.0f;
+//        private float _smooth = 5.0f;
+//        private float tiltAngle = 50.0f;
 
         
-        //float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+//        //float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
 
-        Quaternion _target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+//        Quaternion _target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
 
-        private void Start()
-        {
-/*          input.MovementLeftEvent += HandleMoveLeft;
-            input.MovementLeftEvent += HandleCancelledMoveLeft;
+//        private void Start()
+//        {
+///*          input.MovementLeftEvent += HandleMoveLeft;
+//            input.MovementLeftEvent += HandleCancelledMoveLeft;
 
-            input.MovementRightEvent += HandleMoveRight;
-            input.MovementRightEvent += HandleCancelledMoveRight;
+//            input.MovementRightEvent += HandleMoveRight;
+//            input.MovementRightEvent += HandleCancelledMoveRight;
 
-            input.MovementUpEvent += HandleMoveUp;
-            input.MovementUpEvent += HandleCancelledMoveUp;
+//            input.MovementUpEvent += HandleMoveUp;
+//            input.MovementUpEvent += HandleCancelledMoveUp;
 
-            input.MovementDownEvent += HandleMoveDown;
-            input.MovementDownEvent += HandleCancelledMoveDown;
+//            input.MovementDownEvent += HandleMoveDown;
+//            input.MovementDownEvent += HandleCancelledMoveDown;
 
-            input.ShootEvent += HandleShoot;
-            input.ShootEvent += HandleCancelledShoot;*/
-        }
+//            input.ShootEvent += HandleShoot;
+//            input.ShootEvent += HandleCancelledShoot;*/
+//        }
 
-        // Update is called once per frame
-        private void Update()
-        {
-            Move();
-            Shoot();
-        }
+//        // Update is called once per frame
+//        private void Update()
+//        {
+//            Move();
+//            Shoot();
+//        }
 
-        private void HandleMoveLeft()
-        {
-            _moveLeft = true;
-        }
+//        private void HandleMoveLeft()
+//        {
+//            _moveLeft = true;
+//        }
 
-        private void HandleCancelledMoveLeft()
-        {
-            _moveLeft = false;
-        }
+//        private void HandleCancelledMoveLeft()
+//        {
+//            _moveLeft = false;
+//        }
 
-        private void HandleMoveRight()
-        {
-            _moveRight = true;
-        }
+//        private void HandleMoveRight()
+//        {
+//            _moveRight = true;
+//        }
 
-        private void HandleCancelledMoveRight()
-        {
-            _moveRight = false;
-        }
+//        private void HandleCancelledMoveRight()
+//        {
+//            _moveRight = false;
+//        }
 
-        private void HandleMoveUp()
-        {
-            _moveUp = true;
-        }
+//        private void HandleMoveUp()
+//        {
+//            _moveUp = true;
+//        }
 
-        private void HandleCancelledMoveUp()
-        {
-            _moveUp = false;
-        }
+//        private void HandleCancelledMoveUp()
+//        {
+//            _moveUp = false;
+//        }
 
-        private void HandleMoveDown()
-        {
-            _moveDown = true;
-        }
+//        private void HandleMoveDown()
+//        {
+//            _moveDown = true;
+//        }
 
-        private void HandleCancelledMoveDown()
-        {
-            _moveDown = false;
-        }
+//        private void HandleCancelledMoveDown()
+//        {
+//            _moveDown = false;
+//        }
 
-        private void HandleShoot()
-        {
-            _isShooting = true;
-        }
+//        private void HandleShoot()
+//        {
+//            _isShooting = true;
+//        }
 
-        private void HandleCancelledShoot()
-        {
-            _isShooting = false;
-        }
+//        private void HandleCancelledShoot()
+//        {
+//            _isShooting = false;
+//        }
 
-        private void Move()
-        {
-            // if (_moveLeft)
-            //{
-            // transform.position += Vector3.MoveTowards(transform.position, point3.transform.position, (speed * Time.deltaTime));
-            //}
-            if (left.action.inProgress)
-            {
-                Debug.Log("left");
+//        private void Move()
+//        {
+//            // if (_moveLeft)
+//            //{
+//            // transform.position += Vector3.MoveTowards(transform.position, point3.transform.position, (speed * Time.deltaTime));
+//            //}
+//            if (left.action.inProgress)
+//            {
+//                Debug.Log("left");
                 
-                transform.position -= new Vector3(speed * Time.fixedDeltaTime, 0, 0);
+//                transform.position -= new Vector3(speed * Time.fixedDeltaTime, 0, 0);
 
-                //Vector3 rotation = transform.eulerAngles;
-                //rotation.z -= 10f;
-                //rotation.z = Mathf.Clamp(rotation.x, -45, 45);
-                //rotation.y = 0f;
-                //rotation.x = 0f;
-                //transform.eulerAngles = rotation;
+//                //Vector3 rotation = transform.eulerAngles;
+//                //rotation.z -= 10f;
+//                //rotation.z = Mathf.Clamp(rotation.x, -45, 45);
+//                //rotation.y = 0f;
+//                //rotation.x = 0f;
+//                //transform.eulerAngles = rotation;
 
-                //Quaternion _target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+//                //Quaternion _target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
 
-                //transform.rotation = Quaternion.Slerp(transform.rotation, _target, Time.deltaTime * _smooth);
+//                //transform.rotation = Quaternion.Slerp(transform.rotation, _target, Time.deltaTime * _smooth);
 
-            }
-            /*if (right.action.triggered)
-            {
-                Debug.Log("right");
-                transform.position = Vector3.Lerp(transform.position, point3.transform.position, (speed * Time.deltaTime));
-            }*/
-            if (right.action.inProgress)
-            {
-                Debug.Log("right");
+//            }
+//            /*if (right.action.triggered)
+//            {
+//                Debug.Log("right");
+//                transform.position = Vector3.Lerp(transform.position, point3.transform.position, (speed * Time.deltaTime));
+//            }*/
+//            if (right.action.inProgress)
+//            {
+//                Debug.Log("right");
 
-                float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+//                float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
 
-                transform.position += new Vector3(speed * Time.fixedDeltaTime, 0, 0);
+//                transform.position += new Vector3(speed * Time.fixedDeltaTime, 0, 0);
 
-                transform.rotation = Quaternion.Slerp(transform.rotation, _target, Time.deltaTime * _smooth);
-
-
-                //Vector3 rotation = transform.eulerAngles;
-                //rotation.z += 10f;
-                //rotation.z = Mathf.Clamp(rotation.x, -45, 45);
-                //rotation.y = 0f;
-                //rotation.x = 0f;
-                //transform.eulerAngles = rotation;
-
-            }
+//                transform.rotation = Quaternion.Slerp(transform.rotation, _target, Time.deltaTime * _smooth);
 
 
-        }
+//                //Vector3 rotation = transform.eulerAngles;
+//                //rotation.z += 10f;
+//                //rotation.z = Mathf.Clamp(rotation.x, -45, 45);
+//                //rotation.y = 0f;
+//                //rotation.x = 0f;
+//                //transform.eulerAngles = rotation;
 
-        private void Shoot()
-        {
-            if (shoot.action.triggered)
-            {
-                Debug.Log("shoot");
-            }
-        }
-    }
-}
+//            }
+
+
+//        }
+
+//        private void Shoot()
+//        {
+//            if (shoot.action.triggered)
+//            {
+//                Debug.Log("shoot");
+//            }
+//        }
+//    }
+//}
