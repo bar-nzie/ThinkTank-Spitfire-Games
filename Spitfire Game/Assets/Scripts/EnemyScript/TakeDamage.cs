774208d239770a7878757764cf01fba01b9b2601
James Roper <145114710+WookiePub3@users.noreply.github.com>
2025-04-17 00:41:56 +0100

    added balloon sound effects
    e

--- Spitfire Game/Assets/Scripts/EnemyScript/TakeDamage.cs
+++ Spitfire Game/Assets/Scripts/EnemyScript/TakeDamage.cs
@@ -10,16 +10,38 @@ public class TakeDamage : MonoBehaviour
 
     public GameObject explosionPrefab;
     public int Health;
+    private bool isDying = false;
 
     [SerializeField] AudioSource takeDamageSound;
+    [SerializeField] AudioSource balloonPopSound;
 
     [SerializeField] float minPitch = 0.95f;
     [SerializeField] float maxPitch = 1.05f;
 
+    //Pitch shift
     void PlayTakeDamageSound()
     {
-        takeDamageSound.pitch = Random.Range(minPitch, maxPitch);
-        takeDamageSound.Play();
+        if (takeDamageSound != null)
+        {
+            takeDamageSound.pitch = Random.Range(minPitch, maxPitch);
+            takeDamageSound.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource not assigned for TakeDamage sound!");
+        }
+    }
+    void PlayBalloonPopSound()
+    {
+        if (balloonPopSound != null)
+        {
+            balloonPopSound.pitch = Random.Range(minPitch, maxPitch);
+            balloonPopSound.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource not assigned for BalloonPop sound!");
+        }
     }
 
     private void Awake()
@@ -29,12 +51,22 @@ public class TakeDamage : MonoBehaviour
     }
     public void takeDamage()
     {
+        //Prevent further damage after being "killed"
+        if (isDying) return;
+
         Health -= 1;
         if (Health <= 0)
         {
+            //Mark as dying
+            isDying = true;
+            //Turn invisible while dying and disable collisions
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+
+            PlayBalloonPopSound();
             Score.Instance.scoreIncreaser();
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, balloonPopSound.clip.length);
         }
         StartCoroutine(FlashEffect());
         PlayTakeDamageSound();
